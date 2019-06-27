92d027095f2a2e9265170742db8b47c383a2f9c7
HilkiahMakemo <hilkiah.makemo@skywest.com>
2019-06-27 10:25:31 -0600

    Update HomeFootsy.php

--- src/HomeFootsy.php
+++ src/HomeFootsy.php
@@ -13,34 +13,34 @@ class HomeFootsy extends DataObject {
 	private static $db = array(
 		'LinkTitle' => 'Varchar(50)'
 	);
-	
+
 	private static $has_one = array(
-		'Link' => 'SiteTree',
-		'HomePage' => 'HomePage'
+		'Link' => SiteTree::class,
+		'HomePage' => HomePage::class,
 	);
-	
+
 	private static $singular_name = 'HomeFootsy';
 	private static $plural_name	  = 'HomeFootsies';
-	
+
 	private static $summary_fields = array(
 		'LinkTitle' => 'LinkTitle',
 		'Link.Title' => 'Link'
 	);
-	
+
 	public function getCMSFields() {
 		$f = new FieldList();
-		
+
 		$fLink = new TreeDropdownField('LinkID', 'Link to Page', 'SiteTree');
         //$fLink->setEmptyString('Select One...');
-        
+
         $f->push(TextField::create('LinkTitle')->setTitle('Link Title'));
         $f->push($fLink);
-        
+
         $f->removeByName('SortOrder');
-        
+
 		return $f;
 	}
-	
+
 	function FooterLink() {
 		if($this->LinkID) {
 			//debug::show($this->LinkID);
@@ -49,10 +49,10 @@ class HomeFootsy extends DataObject {
 			//debug::show($pageLink->Link());
 		}
 	}
-	
+
 	/**
 	 * onBeforeWrite function checks to see if title is filled out, if not put the page title from the dropdown in
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
